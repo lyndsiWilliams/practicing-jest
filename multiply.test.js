4fca06dfb9271e01eb6cc96b7b1167730c0eaebb
lyndsiWilliams <kcatgirl@gmail.com>
2021-01-21 10:42:56 -0600

    Lesson 7 complete

--- multiply.test.js
+++ multiply.test.js
@@ -2,7 +2,8 @@ const multiply = require('./multiply');
 
 describe('Test multiply positive scenarios', () => {
   // You can use either 'it' or 'test' to initiate the test
-  test('multiply 3*2 should equal 6', () => {
+  // .only makes it so that all other tests are skipped
+  test.only('multiply 3*2 should equal 6', () => {
     expect(multiply(3, 2)).toBe(6);
   });
 });
