2773dc55090fe7534d7614b2ccf87bf1d7435bd3
lyndsiWilliams <kcatgirl@gmail.com>
2021-01-21 10:04:10 -0600

    Lesson 4 complete

--- multiply.test.js
+++ multiply.test.js
@@ -2,7 +2,18 @@ const multiply = require('./multiply');
 
 describe('Test multiply positive scenarios', () => {
   // You can use either 'it' or 'test' to initiate the test
-  test('multiply 3 * 2 should equal 6', () => {
+  test('multiply 3*2 should equal 6', () => {
     expect(multiply(3, 2)).toBe(6);
   });
+});
+
+test('object example', () => {
+  const data = { first: 1 };
+  data['second'] = 2;
+  // toBe would fail this test
+  expect(data).toEqual({ first: 1, second: 2 });
+});
+
+test('multiply 4*3 should not equal 11', () => {
+  expect(multiply(4,3)).not.toBe(11);
 });
